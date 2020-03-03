fc5d44465b82d25009ab259f6e7af76294dc5d69
Miles <milesmajefski@gmail.com>
2020-03-02 18:41:14 -0600

    asrf

--- socket_com.py
+++ socket_com.py
@@ -18,7 +18,11 @@ if MODE == 's':
         # now our endpoint knows about the OTHER endpoint.
         clientsocket, address = s.accept()
         print(f"Connection from {address} has been established.")
+        clientsocket.send(bytes("Hey there!!!","utf-8"))
+
 elif MODE == 'c':
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(('192.168.1.176', 1234))
+    msg = s.recv(1024)
+    print(msg.decode("utf-8"))
 
