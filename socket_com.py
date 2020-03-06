3f42f6a4e42262233709930cefe7e06159ee12c3
Miles <milesmajefski@gmail.com>
2020-03-05 20:22:24 -0600

    looped

--- socket_com.py
+++ socket_com.py
@@ -25,11 +25,15 @@ if MODE == "s":
         # now our endpoint knows about the OTHER endpoint.
         clientsocket, address = s.accept()
         print(f"Connection from {address} has been established.")
-        clientsocket.send(bytes("Hey there!!!", "utf-8"))
+        while True:
+            clientsocket.send(bytes("Hey there!!!", "utf-8"))
+            import time
+            time.sleep(3)
 
 elif MODE == "c":
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect(("192.168.1.176", 1234))
-    msg = s.recv(1024)
-    print(msg.decode("utf-8"))
+    while True:
+        msg = s.recv(1024)
+        print(msg.decode("utf-8"))
 
