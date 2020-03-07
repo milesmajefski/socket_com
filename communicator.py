10f8162556bfba235c05ed26caf04df5a1ac0676
Miles <milesmajefski@gmail.com>
2020-03-07 13:52:59 -0600

    dang

--- communicator.py
+++ communicator.py
@@ -35,7 +35,7 @@ class Communicator:
     async def handle_outgoing(self):
         """"""
         for host, conn in self.outgoing_connections.items():
-            conn.send(bytes("Hey there!!!", "utf-8"))
+            conn.send(bytes("Hey there !!!", "utf-8"))
             asyncio.sleep(3)
 
 
