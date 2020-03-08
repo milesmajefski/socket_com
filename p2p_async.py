5882e406071fed350a4b9f4e06d769a3754ce8ad
Miles <milesmajefski@gmail.com>
2020-03-08 10:35:08 -0500

    added client

--- p2p_async.py
+++ p2p_async.py
@@ -1,16 +1,45 @@
 import asyncio
+import socket
+
+config = {}
 
 
 async def handle_client(reader, writer):
     request = None
-    while request != 'quit':
-        request = (await reader.read(255)).decode('utf8')
-        response = str(eval(request)) + '\n'
-        writer.write(response.encode('utf8'))
+    while request != "quit":
+        request = (await reader.read(255)).decode("utf8")
+        response = str(eval(request)) + "\n"
+        writer.write(response.encode("utf8"))
         await writer.drain()
     writer.close()
 
+
+async def make_requests():
+    message = f"hi from {config['local_IP']}"
+    while True:
+        reader, writer = await asyncio.open_connection(config['remote_IP'], 15555)
+
+        print(f"Send: {message!r}")
+        writer.write(message.encode())
+
+        data = await reader.read(100)
+        print(f"Received: {data.decode()!r}")
+
+        print("Close the connection")
+        writer.close()
+
+        asyncio.sleep(3)
+
+
+def setup():
+    global config
+    config["local_IP"] = socket.gethostbyname(socket.gethostname())
+    config["remote_IP"] = input("IP to connect to: ")
+
+
 if __name__ == "__main__":
+    setup()
     loop = asyncio.get_event_loop()
-    loop.create_task(asyncio.start_server(handle_client, 'localhost', 15555))
+    loop.create_task(asyncio.start_server(handle_client, "localhost", 15555))
+    loop.create_task(asyncio.run(make_requests()))
     loop.run_forever()
